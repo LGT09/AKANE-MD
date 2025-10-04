6efe7da58bbe7301fbb3f3dfdad5d4e25ef56f12
Vincent Ganiza <vincentgaga46@gmail.com>
2025-10-04 12:41:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*
- * Base By Richie 
- * Created On 11/10/2024
- * Contact Me on tg t.me/hmmletts 
+ * Base By Lil Gaga 
+ * Created On 03/10/2025
+ * Contact Me on tg t.me/Xaphnathpanior 
 */
 
 const chalk = require("chalk")
@@ -9,16 +9,16 @@ const fs = require("fs")
 require('dotenv').config(); 
 
 //================= { SETTINGS } =================\\
-global.owner = process.env.OWNER_NUMBER || "234"; // owner number 
-global.botimg = process.env.BOT_IMG || "https://files.catbox.moe/l9gpzm.jpg"
+global.owner = process.env.OWNER_NUMBER || "263716857999"; // 263716857999 
+global.botimg = process.env.BOT_IMG || "https://files.catbox.moe/hvljlp.jpg"
 global.previewLink = process.env.PREVIEW_LINK || ""
 global.sudo = process.env.SUDO  || " ";
-global.ownername = process.env.OWNER_NAME || "Richie";
-global.botname = process.env.BOT_NAME || "AKANE"
+global.ownername = process.env.OWNER_NAME || "Lil Gaga";
+global.botname = process.env.BOT_NAME || "Gaga09XMD"
 global.onlypc = process.env.ONLYPC_MSG  || "Sorry buddy the bot won't work directly in group chat to reduce spam use in private chat";
 global.onlygc = process.env.ONLYGC_MSG  || "Sorry buddy the bot won't work directly in private chat to reduce spam use in group";
-global.session = process.env.SESSION_ID || ""
-global.timezone = process.env.TIME_ZONE || "Africa/Lagos";
+global.session = process.env.SESSION_ID || "Akane-Md_d334587fa61c07a307387a75ee2960d8"
+global.timezone = process.env.TIME_ZONE || "Africa/Harare";
 
 
 //======= Don't touch =======\\
@@ -41,7 +41,7 @@ global.loc = "1"
 global.domain = "add_domaim_url"
 global.apikey = "add_api_key"
 global.capikey = "add_capikey"
-global.creatorName = "Richie"
+global.creatorName = "LilGaga"
 
 //==========================
     
